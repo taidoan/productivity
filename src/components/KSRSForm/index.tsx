4ce68a6e25e60368926267b581d476f114696815
Tai Doan <taidoan@live.co.uk>
2025-08-18 13:47:27 +0100

    feat(v3): Started option to show floor and kitchen lates

--- src/components/KSRSForm/index.tsx
+++ src/components/KSRSForm/index.tsx
@@ -11,6 +11,8 @@ type KSRSFormProps = {
     lateTarget: number;
     prepTarget: number;
     foodLift: boolean;
+    kitLates: boolean;
+    floorLates: boolean;
     copiedServiceData: string;
     copiedProdData: string;
   }
@@ -22,10 +24,15 @@ const KSRSForm = ({ onSubmit, initialValues}: KSRSFormProps) => {
   const [lateTarget, setLateTarget] = useState<number>(25);
   const [prepTarget, setPrepTarget] = useState<number>(8);
   const [lift, setLift] = useState<boolean>(false);
+  const [kitLates, setKitLates] = useState<boolean>(false);
+  const [floorLates, setFloorLates] = useState<boolean>(false);
   const [serviceData, setServiceData] = useState<string>('');
   const [prodData, setProdData] = useState<string>('');
   const [copiedServiceData, setCopiedServiceData] = useState<string>('');
   const [copiedProdData, setCopiedProdData] = useState<string>('');
+
+
+
   useEffect(() => {
     if (initialValues) {
       setSales(initialValues.sales);
@@ -33,6 +40,8 @@ const KSRSForm = ({ onSubmit, initialValues}: KSRSFormProps) => {
       setLateTarget(initialValues.lateTarget);
       setPrepTarget(initialValues.prepTarget);
       setLift(initialValues.foodLift);
+      setKitLates(initialValues.kitLates);
+      setFloorLates(initialValues.floorLates);
       setServiceData(initialValues.copiedServiceData);
       setProdData(initialValues.copiedProdData);
       setCopiedServiceData(initialValues.copiedServiceData);
@@ -60,6 +69,14 @@ const KSRSForm = ({ onSubmit, initialValues}: KSRSFormProps) => {
 
   const handleLiftChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setLift(event.target.checked);
+  };
+
+  const handleKitLateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setKitLates(event.target.checked);
+  };
+
+  const handleFloorLateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setFloorLates(event.target.checked);
   };
 
   const handleServiceChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
@@ -404,17 +421,43 @@ const KSRSForm = ({ onSubmit, initialValues}: KSRSFormProps) => {
             <option value={9}>9:00</option>
           </select>
         </div>
+      </div>
 
-        <div className={`${formFieldClass} !flex-row gap-x-3 justify-center`}>
-          <label htmlFor="foodLift" className={`${labelClass}`}>Food Lift: <span className="text-sm font-normal text-grey-400">(Optional)</span></label>
-          <input 
-            type="checkbox"
-            id="foodLift"
-            checked={lift} 
-            onChange={handleLiftChange}
-            className="accent-primary-600 focus:accent-primary-600"
-          />
-        </div>
+      <div className={`flex flex-col gap-4 flex-wrap w-full md:flex-row md:col-span-4 `}>
+
+          <div className={`${formFieldClass} !flex-row gap-x-3 md:justify-center`}>
+            <label htmlFor="kitchenLates" className={`${labelClass}`}>Kitchen Lates: <span className="text-sm font-normal text-grey-400">(Optional)</span></label>
+            <input 
+              type="checkbox"
+              id="kitchenLates"
+              checked={kitLates} 
+              onChange={handleKitLateChange}
+              className="accent-primary-600 focus:accent-primary-600"
+            />
+          </div>
+
+          <div className={`${formFieldClass} !flex-row gap-x-3 md:justify-center`}>
+            <label htmlFor="floorLates" className={`${labelClass}`}>Floor Lates: <span className="text-sm font-normal text-grey-400">(Optional)</span></label>
+            <input 
+              type="checkbox"
+              id="floorLates"
+              checked={floorLates} 
+              onChange={handleFloorLateChange}
+              className="accent-primary-600 focus:accent-primary-600"
+            />
+          </div>
+
+          <div className={`${formFieldClass} !flex-row gap-x-3 md:justify-center`}>
+            <label htmlFor="foodLift" className={`${labelClass}`}>Food Lift: <span className="text-sm font-normal text-grey-400">(Optional)</span></label>
+            <input 
+              type="checkbox"
+              id="foodLift"
+              checked={lift} 
+              onChange={handleLiftChange}
+              className="accent-primary-600 focus:accent-primary-600"
+            />
+          </div>
+        
       </div>
 
       <div className={`${formFieldClass} col-span-4`}>
